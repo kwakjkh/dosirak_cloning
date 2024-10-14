379b67bc0a64cd6ec7a0b13510daacf3080c16a3
kwakjkh <1210kwakjkh@gmail.com>
2024-10-14 17:28:53 +0900

    수정본 새로 업로드

--- js/business.js
+++ js/business.js
@@ -1,14 +1,14 @@
-window.addEventListener("load",function(){
-    const swBusiness = new Swiper(".sw-business",{
-        slidesPerView:1,
-        spaceBetween:0,
-        breakpoints:{
-            640:{
-                slidesPerView:2,
-            }, 1024:{
-                slidesPerView:3,
-            },
-        },
-    });
-    
+window.addEventListener("load", function () {
+  const swBesiness = new Swiper(".sw-business", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
 });
